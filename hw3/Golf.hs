676eb4c6d8adac529d335d5867c299ada25a96ce
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-01 12:10:00 +0800

    done ex3.

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import qualified Data.List as L
+
 -- ex1
 skips :: [a] -> [[a]]
 skips [] = []
@@ -13,9 +15,21 @@ localMaxima [_] = []
 localMaxima [_, _] = []
 localMaxima (x:y:z:xs) = [y | x<y, z<y] ++ localMaxima (y:z:xs)
 
+-- ex3
+histogram :: [Integer] -> String
+histogram x = concat $ L.intersperse "\n" $ line x
+  where line x =  L.reverse $ L.transpose $ pair x
+        pair x = map (\n -> show n ++ fill x !! n) [0..9]
+        fill x = map (\n -> replicate n '*' ++ replicate ((maximum $ occur x) - n) ' ') $ occur x
+        occur x = map (\n -> length $ filter (==n) x ) [0..9]
+                
+vshow x = putStrLn $ histogram x
+
 -- test
 str1 = "abcd"
+
 -- localMaxima [2,9,5,6,1] == [9,6]
 -- localMaxima [2,3,4,1,5] == [4]
 -- localMaxima [1,2,3,4,5] == []
 
+-- vshow [99,9,1,2,3,2,3]
