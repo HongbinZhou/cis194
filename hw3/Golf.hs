5d87e0ded8e2f084478e49c527b7a09a5bbfa117
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-01 09:28:47 +0800

    rework hw3 ex1

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,18 +1,11 @@
-{-# LANGUAGE TemplateHaskell #-}
 module Golf where
 
 -- ex1
 skips :: [a] -> [[a]]
-skips a = map (getStrByIdx a) [1 .. length a]
-
-getStrByIdx :: [a] -> Int -> [a]
-getStrByIdx s 1 = s
-getStrByIdx s n = map (s !!) $ genIdx n $ length s
-
-genIdx :: Int->Int->[Int]
-genIdx 1 _ = []
-genIdx step len = map (subtract 1) [step, step*2 .. len]
-
+skips [] = []
+skips a = [a] ++ map getByIdx [2 .. length a]
+          where getByIdx n = map (\x -> a !! (x-1)) [n, n*2 .. length a]
+                
 -- test
 str1 = "abcd"
 
