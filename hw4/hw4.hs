fc0ac108630f47590fa4e5d7db92405186c5820e
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-04 21:34:00 +0800

    done ex2

--- hw4/hw4.hs
+++ hw4/hw4.hs
@@ -19,3 +19,19 @@ fun2' =  sum . filter even . takeWhile (>1) . iterate f
       where f n 
               | even n = n `div` 2
               | otherwise = 3 * n + 1
+-- ex2
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+     deriving (Show, Eq)
+
+-- foldTree :: [a] -> Tree a
+foldTree = foldr addNode Leaf 
+         where addNode a Leaf = Node 1 Leaf a Leaf
+               addNode a (Node n leftTree b rightTree) 
+                         | depth leftTree <= depth rightTree = (Node (n+1) (addNode a leftTree) b rightTree)
+                         | otherwise = (Node n leftTree b (addNode a rightTree))
+                         where depth Leaf = 0
+                               depth (Node n _ _ _) = n
+        
+-- test
+-- foldTree "ABCDEFGHIJ"
