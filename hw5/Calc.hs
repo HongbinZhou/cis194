d7a1813b53ba6d24149c998ea634db1681d207eb
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-05 22:26:22 +0800

    done ex3.

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -19,6 +19,19 @@ evalStr x = evalMaybe (parseExp Lit Add Mul x)
 -- another version. Use fmap for functor.
 evalStr' x = fmap eval (parseExp Lit Add Mul x)
 
+
+-- ex3
+class Expr a where
+      lit :: Integer -> a
+      add :: a -> a -> a
+      mul :: a -> a -> a
+
+instance Expr ExprT where
+         lit x = Lit x
+         add x y = Add x y
+         mul x y = Mul x y
+
+
 -- test
 -- (2+3) * 4
 main = eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20
