226ee73948d3bc16913267954004bb0559fb43db
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-05 22:10:33 +0800

    done ex2

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -2,12 +2,22 @@
 module Calc where
 
 import ExprT
+import Parser
 
 -- ex1
 eval :: ExprT -> Integer
 eval (Lit a) = a
 eval (Add x y) = eval x + eval y
 eval (Mul x y) = eval x * eval y
+
+-- ex2
+-- evalStr :: String -> Maybe Integer
+evalStr x = evalMaybe (parseExp Lit Add Mul x)
+        where evalMaybe Nothing = Nothing
+              evalMaybe (Just a) = Just (eval a)
+
+-- another version. Use fmap for functor.
+evalStr' x = fmap eval (parseExp Lit Add Mul x)
 
 -- test
 -- (2+3) * 4
