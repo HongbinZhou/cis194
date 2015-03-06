7251198bc5d343af5d20979e14bae17889c79171
Hongbin Zhou <hongbinzhou@gmail.com>
2015-03-06 17:41:49 +0800

    try ex5 of hw5, not done yet.

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -1,23 +1,26 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Calc where
 
 import ExprT
 import Parser
+import StackVM
 
 -- ex1
 eval :: ExprT -> Integer
 eval (Lit a) = a
-eval (Add x y) = eval x + eval y
-eval (Mul x y) = eval x * eval y
+eval (ExprT.Add x y) = eval x + eval y
+eval (ExprT.Mul x y) = eval x * eval y
 
 -- ex2
 -- evalStr :: String -> Maybe Integer
-evalStr x = evalMaybe (parseExp Lit Add Mul x)
+evalStr x = evalMaybe (parseExp ExprT.Lit ExprT.Add ExprT.Mul x)
         where evalMaybe Nothing = Nothing
               evalMaybe (Just a) = Just (eval a)
 
 -- another version. Use fmap for functor.
-evalStr' x = fmap eval (parseExp Lit Add Mul x)
+evalStr' x = fmap eval (parseExp ExprT.Lit ExprT.Add ExprT.Mul x)
 
 
 -- ex3
@@ -28,10 +31,11 @@ class Expr a where
 
 
 instance Expr ExprT where
-         lit a = Lit a
-         mul x y = Mul x y
-         add x y = Add x y
+         lit a = ExprT.Lit a
+         mul x y = ExprT.Mul x y
+         add x y = ExprT.Add x y
 
+-- ex4
 instance Expr Integer where
          lit = id
          add = (+)
@@ -55,8 +59,20 @@ instance Expr Mod7 where
          mul (Mod7 a) (Mod7 b) = Mod7 ( (a*b) `mod` 7)
 
 
+-- ex5
+instance Expr Program where
+         lit x = [StackVM.PushI x]
+         add a b = a ++ b ++ [StackVM.Add]
+         mul a b = a ++ b ++ [StackVM.Mul]
+
+-- compile :: String -> Maybe Program
+-- This can work now: 
+test = (mul (add (lit 1) (lit 2)) (lit 2)) :: Program
+-- compile = parseExp StackVM.IVal StackVM.Add StackVM.Mul 
+
 -- test
 -- (2+3) * 4
-main = eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20
+-- main = eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20
+
 
 
