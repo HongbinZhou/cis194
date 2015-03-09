61f66396e1668364a6da63b6da29b78c8467456b
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-09 23:28:16 +0800

    part of ex2.

--- hw6/Fibonacci.hs
+++ hw6/Fibonacci.hs
@@ -5,5 +5,15 @@ fib 0 = 0
 fib 1 = 1
 fib n = (fib (n-1)) + (fib (n-2))
 
-fibsl :: [Integer]
-fibsl = map fib [0..]
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
+
+-- fibs2 :: [Integer]
+fibs2 0 = [0]
+fibs2 1 = [1,0]
+fibs2 n = (x+y):xs
+       where xs = fibs2 (n-1)
+             x = head xs
+             y = head $ tail xs
+
+fibs2' = map fibs2 [0..]
