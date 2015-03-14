e1d49a80523ea3f33b42b26b62537fd4473e0e69
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-14 22:55:58 +0800

    Add lectures in case the original weisite down.
    ref: http://www.seas.upenn.edu/~cis194/spring13/lectures.html.

--- ex-pdf/getEx.sh
+++ ex-pdf/getEx.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-for i in $( cat ../lectures.html| grep 'hw\/' | awk -F= '{print $2}' | awk -F\" '{print $2}' ); do wget http://www.seas.upenn.edu/\~cis194/spring13/$i; done
+for i in $( cat lectures.html| grep 'hw\/' | awk -F= '{print $2}' | awk -F\" '{print $2}' ); do wget http://www.seas.upenn.edu/\~cis194/spring13/$i; done
+
+for i in $( cat lectures.html| grep 'lectures\/' | awk -F= '{print $2}' | awk -F\" '{print $2}' ); do wget http://www.seas.upenn.edu/\~cis194/spring13/$i; done
