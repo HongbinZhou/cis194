3be5a5a4ccae89e403301e50c685eb63553cbfc7
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-14 21:26:03 +0800

    done ex3 of hw8.

--- hw8/Party.hs
+++ hw8/Party.hs
@@ -31,6 +31,7 @@ moreFun gl0@(GL _ f0) gl1@(GL _ f1)
         | f0 > f1 = gl0
         | otherwise = gl1
 
+-- ex2
 treeFold ::  (a -> b -> b) -> b -> Tree a -> b
 treeFold f b (Node a nodes) = 
          foldr (\x acc -> treeFold f acc x) (f a b) nodes
@@ -38,3 +39,10 @@ treeFold f b (Node a nodes) =
 n = Node 2 [(Node 0 []), (Node 1 [])]
 
 main = treeFold (+) 1 n
+
+-- ex3
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss l = (withBoss, withoutBoss)
+          where withBoss = glCons boss mempty
+                withoutBoss = foldr f mempty l
+                            where f (_, b) acc = moreFun acc (glCons boss b)
